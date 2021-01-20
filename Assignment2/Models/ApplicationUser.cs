1f664a7197d16ffd1f36b8bd11222f26166cb672
Brodey Yendall <S3718834@student.rmit.edu.au>
2021-01-20 15:19:30 +1100

    Attempting to use a custom user

---
+++ Assignment2/Models/ApplicationUser.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Assignment2.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+        public string CustomerTag { get; set; }
+    }
+}
