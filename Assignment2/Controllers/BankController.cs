435eb6308ef360245275017eed3c030c9677aec1
rmit-s3718036-dylan-sbogar <37315610+DylanSbogar@users.noreply.github.com>
2021-01-20 19:16:04 +1100

    Made skeleton for ATM page

---
+++ Assignment2/Controllers/BankController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Assignment2.Data;
+using Assignment2.Models;
+using Microsoft.AspNetCore.Authorization;
+
+namespace Assignment2.Controllers
+{
+    public class BankController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public BankController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Account.ToListAsync());
+        }
+    }
+}
