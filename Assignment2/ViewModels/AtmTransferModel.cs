4d70ea542cfd668205dbfd9c79c3a784334ee1ab
rmit-s3718036-dylan-sbogar <37315610+DylanSbogar@users.noreply.github.com>
2021-01-21 13:12:48 +1100

    Implemented transfer function. Still requires validation on destination account number

---
+++ Assignment2/ViewModels/AtmTransferModel.cs
@@ -0,0 +1,14 @@
+﻿using Assignment2.Models;
+
+namespace Assignment2.ViewModels
+{
+    public class AtmTransferModel
+    {
+        public int AccountNumber { get; set; }
+        public int DestinationAccountNumber { get; set; }
+        public Account Account { get; set; }
+        public Account DestinationAccount { get; set; }
+        public decimal Amount { get; set; }
+        public string Comment { get; set; }
+    }
+}
