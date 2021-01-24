c97bfb8abd89f05d091fe07112d4fde694c16841
Brodey Yendall <S3718834@student.rmit.edu.au>
2021-01-24 18:48:48 +1100

    Implemented method that gets data needed for an activity report

---
+++ Assignment2/Models/Data/ActivityReportHistory.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Assignment2.Models.Data
+{
+    public class ActivityReportHistory
+    {
+        [Key, Required]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [Required, DataType(DataType.DateTime)]
+        public DateTime OccuredWhen { get; set; }
+        public int EmailsSent { get; set; }
+    }
+}
--- Assignment2/Models/Email/EmailController.cs
+++ Assignment2/Models/Email/EmailController.cs
@@ -43,9 +43,56 @@ namespace Assignment2.Models
                     Transactions = account.Transactions
                 });
             }
-
-            await _emailSender.SendActivityReportAsync("brodeyyendall@gmail.com", await GenerateActivityReportHtml(activityReportModels));
+            await GetActivityReportsSince(DateTime.MinValue);
+            //await _emailSender.SendActivityReportAsync("brodeyyendall@gmail.com", await GenerateActivityReportHtml(activityReportModels));
             return View("ActivityReport", activityReportModels);
+        }
+
+        public async Task<List<ActivityReportModel>> GetActivityReportsSince(DateTime dateMinimum)
+        {
+            // TODO Find how to make this async
+            var newTransactions = _context.Transaction.Where(transaction => transaction.ModifyDate > dateMinimum)
+                .AsEnumerable()
+                .GroupBy(transaction => transaction.AccountNumber);
+
+
+            // Start all the requests to the database for accessing the account the transasctions belong to.
+            // Done like this so one request doesn't hold up the others and parallelisation reduces work time
+            var activityReportCreationTasks = new List<Task<ActivityReportModel>>();
+            foreach(var transactionGroup in newTransactions)
+            {
+                activityReportCreationTasks.Add(ConvertToActivityReportModel(transactionGroup));
+            }
+
+            // As the accounts come back from the database, add the constructed activity reports to the list
+            var activityReportModels = new List<ActivityReportModel>();
+            while(activityReportCreationTasks.Count > 0)
+            {
+                var finishedTask = await Task.WhenAny(activityReportCreationTasks); // Get a finished task
+                activityReportCreationTasks.Remove(finishedTask); // Remove from the list because it is done
+                activityReportModels.Add(await finishedTask); // Add the result to the list
+            }
+
+
+            return activityReportModels;
+        }
+
+        private async Task<ActivityReportModel> ConvertToActivityReportModel(IGrouping<int, Transaction> groupedTransaction)
+        {
+            var accountTask = _context.Account.Where(account => account.AccountNumber == groupedTransaction.Key).FirstAsync();
+
+            decimal balanceChange = 0;
+            foreach(var transaction in groupedTransaction.ToList())
+            {
+                balanceChange += transaction.Amount;
+            }
+
+            return new ActivityReportModel 
+            { 
+                Account = await accountTask, 
+                Transactions = groupedTransaction.ToList(),
+                BalanceChange = balanceChange
+            };
         }
 
         public async Task<string> GenerateActivityReportHtml(List<ActivityReportModel> model)
