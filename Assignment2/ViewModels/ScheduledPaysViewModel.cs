e324d79bd875b42bc7881abdb916b6f0b800db99
rmit-s3718036-dylan-sbogar <37315610+DylanSbogar@users.noreply.github.com>
2021-01-24 22:04:11 +1100

    Initial file setup for scheduled pay system.

---
+++ Assignment2/ViewModels/ScheduledPaysViewModel.cs
@@ -0,0 +1,19 @@
+﻿using Assignment2.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Assignment2.ViewModels
+{
+    public class ScheduledPaysViewModel
+    {
+        public int PayeeID { get; set; }
+
+        public decimal Amount { get; set; }
+
+        public DateTime ScheduleDate { get; set; }
+
+        public char Period { get; set; }
+    }
+}
