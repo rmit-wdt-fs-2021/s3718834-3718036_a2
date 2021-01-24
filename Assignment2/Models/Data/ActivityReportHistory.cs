3d9382fb3a2988609457a78cc15acd2c33ae2579
Brodey Yendall <S3718834@student.rmit.edu.au>
2021-01-24 22:17:13 +1100

    Added table for history of activity reports

--- Assignment2/Models/Data/ActivityReportHistory.cs
+++ Assignment2/Models/Data/ActivityReportHistory.cs
@@ -5,7 +5,7 @@ using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace Assignment2.Models.Data
+namespace Assignment2.Models
 {
     public class ActivityReportHistory
     {
