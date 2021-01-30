95e9ecf058f501129c449478ef75b9fe78f3a66a
Brodey Yendall <S3718834@student.rmit.edu.au>
2021-01-30 12:50:42 +1100

    Added initial admin site structure

---
+++ AdminSite/Controllers/AdminController.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+using AdminSite.ViewModels;
+using Assignment2.Controllers;
+using Assignment2.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+
+namespace AdminSite.Controllers
+{
+    public class AdminController : Controller
+    {
+        private readonly ILogger<AdminController> _logger;
+        private readonly IHttpClientFactory _clientFactory;
+        private HttpClient _client;
+
+        public AdminController(IHttpClientFactory clientFactory)
+        {
+            _clientFactory = clientFactory;
+            _client = _clientFactory.CreateClient("api");
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            return await Transactions(startDate: DateTime.Today,endDate: DateTime.Today.AddDays(1));
+        }
+
+        public async Task<IActionResult> Transactions(int? customerId = null, DateTime? startDate = null, DateTime? endDate = null)
+        {
+            startDate ??= DateTime.MinValue;
+            endDate ??= DateTime.MaxValue;
+
+            var response = await _client.GetAsync("api/movies/Transactions");
+
+            if(!response.IsSuccessStatusCode)
+            {
+                throw new Exception(); // TODO Get a better exception
+            }
+
+            var result = await response.Content.ReadAsStringAsync();
+
+            var transactionViewModel = new TransactionsViewModel {
+                CustomerId = customerId,
+                StartDate = startDate,
+                EndDate = endDate,
+                Transactions = JsonConvert.DeserializeObject<List<Transaction>>(result)
+            };
+
+
+            return View(transactionViewModel);
+        }
+
+        public IActionResult Lock()
+        {
+            return View();
+        }
+
+        public IActionResult Block()
+        {
+            return View();
+        }
+    }
+}
---
+++ AdminSite/ViewModels/TransactionsViewModel.cs
@@ -0,0 +1,16 @@
+﻿using Assignment2.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AdminSite.ViewModels
+{
+    public class TransactionsViewModel
+    {
+        public int? CustomerId { get; set; }
+        public DateTime? StartDate { get; set; }
+        public DateTime? EndDate { get; set; }
+        public List<Transaction> Transactions { get; set; }
+    }
+}
