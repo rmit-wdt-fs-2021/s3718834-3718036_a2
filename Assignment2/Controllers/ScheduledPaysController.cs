32dbb78ab809b4165546f552596a8ded753b5b2f
rmit-s3718036-dylan-sbogar <37315610+DylanSbogar@users.noreply.github.com>
2021-01-31 02:46:22 +1100

    Changed way we create new scheduled payments, as of now the error checking when creating a new payment is nonexistent but the creation is a lot more effficient than the previous attempt

--- Assignment2/Controllers/ScheduledPaysController.cs
+++ Assignment2/Controllers/ScheduledPaysController.cs
@@ -51,6 +51,26 @@ namespace Assignment2.Controllers
             return View(transactionHistoryModel);
         }
 
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("BillPayId,AccountNumber,PayeeId,Amount,ScheduleDate,Period,ModifyDate")] BillPay billPay)
+        {
+            if (ModelState.IsValid)
+            {
+                billPay.Status = Status.Waiting;
+                billPay.ModifyDate = DateTime.UtcNow;
+                _context.Add(billPay);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(billPay);
+        }
+
         public async Task<IActionResult> Modify(int billPayId)
         {
             var billPay = await _context.BillPay.FirstOrDefaultAsync(m => m.BillPayId == billPayId);
@@ -70,6 +90,7 @@ namespace Assignment2.Controllers
             {
                 try
                 {
+                    billPay.ModifyDate = DateTime.UtcNow;
                     _context.Update(billPay);
                     await _context.SaveChangesAsync();
                 }
@@ -89,57 +110,6 @@ namespace Assignment2.Controllers
             return View(billPay);
         }
 
-        public async Task<IActionResult> Create(int selectedAccountNumber)
-        {
-            return View(
-                new ScheduledPaysViewModel
-                {
-                    SelectedAccountNumber = selectedAccountNumber,
-                    SelectedAccount = await _dataAccess.GetUserAccount(selectedAccountNumber)
-                });
-        }
-
-        [HttpPost]
-        public async Task<IActionResult> Create([Bind("AccountNumber,PayeeId,Amount,ScheduleDate,Period,ModifyDate")] ScheduledPaysViewModel viewModel)
-        {
-            viewModel.SelectedAccount = await _dataAccess.GetUserAccountWithBillPays(viewModel.SelectedAccountNumber);
-
-            await _dataAccess.AddScheduledPayment(viewModel.SelectedAccount, new BillPay
-            {
-                AccountNumber = viewModel.SelectedAccountNumber,
-                PayeeId = viewModel.BillPay.PayeeId,
-                Amount = viewModel.BillPay.Amount,
-                ScheduleDate = viewModel.BillPay.ScheduleDate,
-                Period = viewModel.BillPay.Period,
-                ModifyDate = DateTime.UtcNow
-            });
-
-            // Check the user has enough money to pay the scheduled payment?
-            if (viewModel.BillPay.Amount > await viewModel.SelectedAccount.Balance(_dataAccess))
-            {
-                ModelState.AddModelError(nameof(viewModel.BillPay.Amount), "Amount must not exceed current balance.");
-                return View(viewModel);
-            }
-
-            // Check whether the withdraw amount has more than 2 decimal places.
-            // Code originally sourced from tutorial 5
-            if (decimal.Round(viewModel.BillPay.Amount, 2) != viewModel.BillPay.Amount) 
-            {
-                ModelState.AddModelError(nameof(viewModel.BillPay.Amount), "Amount cannot have more than 2 decimal places.");
-                return View(viewModel);
-            }
-
-            await viewModel.SelectedAccount.UpdateBalance(viewModel.BillPay.Amount, _dataAccess);
-            await _dataAccess.AddTransaction(viewModel.SelectedAccount, new Transaction
-            {
-                AccountNumber = viewModel.SelectedAccountNumber,
-                TransactionType = TransactionType.BillPay,
-                Amount = viewModel.BillPay.Amount,
-                ModifyDate = DateTime.UtcNow
-            });
-
-            return RedirectToAction(nameof(Index));
-        }
 
         public async Task<IActionResult> Delete(int? billPayId)
         {
